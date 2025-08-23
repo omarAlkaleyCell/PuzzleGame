b719f51f25b617bf6c058d6b0173c2af29786097
Cell <omarmohamed@thebadnomads.com>
2025-08-23 22:52:41 +0300

    Grid Manager and their dependencies

---
+++ Assets/Scripts/PieceShapes.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public enum PieceType
+{
+	LShape,
+	Line3,
+	Line4,
+	Square
+}
+
+public static class PieceShapes
+{
+	public static Vector2Int[] GetShape( PieceType type )
+	{
+		switch (type)
+		{
+			case PieceType.LShape:
+				return new Vector2Int[]
+				{
+					new Vector2Int(0, 0),
+					new Vector2Int(0, 1),
+					new Vector2Int(0, 2),
+					new Vector2Int(1, 0)
+				};
+
+			case PieceType.Line3:
+				return new Vector2Int[]
+				{
+					new Vector2Int(0, 0),
+					new Vector2Int(1, 0),
+					new Vector2Int(2, 0)
+				};
+
+			case PieceType.Line4:
+				return new Vector2Int[]
+				{
+					new Vector2Int(0, 0),
+					new Vector2Int(1, 0),
+					new Vector2Int(2, 0),
+					new Vector2Int(3, 0)
+				};
+
+			case PieceType.Square:
+				return new Vector2Int[]
+				{
+					new Vector2Int(0, 0),
+					new Vector2Int(1, 0),
+					new Vector2Int(0, 1),
+					new Vector2Int(1, 1)
+				};
+
+			default:
+				return new Vector2Int[] { Vector2Int.zero };
+		}
+	}
+}
